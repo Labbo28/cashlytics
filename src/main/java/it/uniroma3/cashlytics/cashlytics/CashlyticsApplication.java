0badce2ebb4c9bc8709b5b9876783126ccd86b0e
labbo <labonia.riccardo@gmail.com>
2025-04-23 11:27:29 +0200

    first commit

---
+++ src/main/java/it/uniroma3/cashlytics/cashlytics/CashlyticsApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.cashlytics.cashlytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashlyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashlyticsApplication.class, args);
+	}
+
+}
