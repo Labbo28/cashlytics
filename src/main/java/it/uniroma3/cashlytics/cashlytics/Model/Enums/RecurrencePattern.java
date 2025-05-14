335827060d8a837fea9cdfc14a8cc22dd2dae4fd
Simone <sim.giacomini1@stud.uniroma3.it>
2025-05-14 12:38:26 +0200

    started work on sign up htmls

--- src/main/java/it/uniroma3/cashlytics/cashlytics/Model/Enums/RecurrencePattern.java
+++ src/main/java/it/uniroma3/cashlytics/cashlytics/Model/Enums/RecurrencePattern.java
@@ -1,10 +1,7 @@
 package it.uniroma3.cashlytics.cashlytics.Model.Enums;
 
 public enum RecurrencePattern {
-    WEEKLY,
-    BIWEEKLY,
-    MONTHLY,
-    YEARLY,
-    DAILY,
+    DAILY, WEEKLY,
+    MONTHLY, YEARLY,
     ONE_TIME
 }
